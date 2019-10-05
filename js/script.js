761a1ec83481e4f0af08360158148aca6682b520
Meet09 <meet.gajjar2809@gmail.com>
2019-10-05 16:11:56 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+var typed = new Typed('#writer', {
+		strings : [
+					'I do Photography',
+					'I focus reality'
+					],
+		loop: true,
+		typeSpeed: 100,
+		backSpeed:10,
+		showCursor: false,
+		backDelay:700,
+	});
+});
